916234290481c043f60a948683c870cb5a42fe5b
RonjaBrathen <164937285+RonjaBrathen@users.noreply.github.com>
2024-03-28 17:19:11 +0100

    Update darkmode.js

--- JS/darkmode.js
+++ JS/darkmode.js
@@ -1,1 +1,7 @@
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
 
